ff43231e616cb4b7559d8a0966aab4d60f721b05
nuttysunday <shivamghodke10000@gmail.com>
2024-10-06 00:11:50 -0400

    pls work ffs

--- src/app/api/route.js
+++ src/app/api/route.js
@@ -3,21 +3,16 @@
 export async function GET(req) {
   console.log(req.url)
   const { searchParams } = new URL(req.url);
-  const category = searchParams.get('category') || 'world';
+  const category = searchParams.get('category') || 'top';
   console.log(category)
   const lang = searchParams.get('lang') || 'en';
-  const country = searchParams.get('country');
+  const country = searchParams.get('country') || 'wo';
+  const apiKey = process.env.NEXT_PUBLIC_NEWS_API_KEY;
   
-  
-  const apiKey = process.env.NEXT_PUBLIC_NEWS_API_KEY;;
-  let url = `https://newsdata.io/api/1/latest?apikey=${apiKey}&category=${category}&language=${lang}&removeduplicate=1`;
 
-  if (country && country !== 'Global') {
-   console.log(country);
-   url += `&country=${country}`;
-   console.log(url)
-  }
- 
+  let url = `https://newsdata.io/api/1/latest?apikey=${apiKey}&category=${category}&language=${lang}&country=${country}&removeduplicate=1`;
+
+
   // Revalidate information every two hours
   const response = await fetch(url, { next: { revalidate: 7200 } });
   
